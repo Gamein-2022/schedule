e8b8cbdd18073465a2dbfe5f115cf14544bba294
AliLordLoss <a.a.ghanati@gmail.com>
2023-05-11 05:39:03 +0330

    -fix: gamein bot eligible orders for offer

--- src/main/java/gamein2/schedule/model/repository/OrderRepository.java
+++ src/main/java/gamein2/schedule/model/repository/OrderRepository.java
@@ -8,7 +8,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("SELECT o FROM Order AS o WHERE o.product.minPrice = o.unitPrice AND o.acceptDate IS NULL AND o.cancelled " +
+    @Query("SELECT o FROM Order AS o WHERE ((o.type = 'SELL' AND o.product.minPrice = o.unitPrice) OR (o.type = 'BUY'" +
+            " AND o.product.maxPrice = o.unitPrice)) AND o" +
+            ".acceptDate IS" +
+            " NULL AND o.cancelled " +
             "= FALSE AND o.submitDate < :time AND o.id NOT IN (SELECT offer.order.id FROM Offer AS offer WHERE offer" +
             ".cancelled = FALSE) AND o.id " +
             "NOT IN (SELECT o2.id FROM Order AS o2 WHERE o2.product = o.product AND o2.acceptDate IS NULL AND o2.type" +
